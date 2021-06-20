af22e42d944dfcb32c782dde9529292e96356a5a
Pan32 <slowthgt@gmail.com>
2021-06-20 01:33:15 +0100

    <S4202121-27> Some more work on the Flux server.

--- src/eapli.base/base.core.server/src/main/java/eapli/base/server/tcp/TcpFluxSrv.java
+++ src/eapli.base/base.core.server/src/main/java/eapli/base/server/tcp/TcpFluxSrv.java
@@ -1,13 +1,12 @@
 package eapli.base.server.tcp;
 
 import eapli.base.server.etc.ProtocolConsts;
-
 import javax.net.ssl.SSLServerSocket;
 import javax.net.ssl.SSLServerSocketFactory;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.net.ServerSocket;
+import java.net.InetAddress;
 import java.net.Socket;
 import java.util.HashMap;
 
@@ -41,8 +40,6 @@ public class TcpFluxSrv extends Thread{
         System.setProperty("javax.net.ssl.keyStore",TRUSTED_STORE);
         System.setProperty("javax.net.ssl.keyStorePassword",KEYSTORE_PASS);
 
-        System.out.println();
-
         SSLServerSocketFactory sslF = (SSLServerSocketFactory) SSLServerSocketFactory.getDefault();
 
         try {
@@ -59,11 +56,12 @@ public class TcpFluxSrv extends Thread{
             try {
                 s = sock.accept();
                 addCli(s);
+                Thread cli = new TCPFluxSrvClient(s);
+                cli.start();
             } catch (Exception e) {
                 e.printStackTrace();
             }
-            Thread cli = new TCPFluxSrvClient(s);
-            cli.start();
+
         }
     }
 }
@@ -79,14 +77,21 @@ class TCPFluxSrvClient extends Thread {
     }
 
     public void run() {
+
+        InetAddress clientIP=myS.getInetAddress();
+        System.out.println("New client connection from " + clientIP.getHostAddress() +
+                ", port number " + myS.getPort());
         int nChars = 0;
         byte[] data = new byte[ProtocolConsts.MESSAGE_SIZE];
 
         try {
+            System.out.println("Trying to read data");
             sIn = new DataInputStream(myS.getInputStream());
             sOut = new DataOutputStream(myS.getOutputStream());
             while (true) {
-                sIn.read(data, 0, nChars);
+                nChars = sIn.read(data, 0, ProtocolConsts.MESSAGE_SIZE);
+                if (nChars == 0)
+                    continue;
                 int version = ParseMessage.getVersion(data);
                 if (version != ProtocolConsts.PROTOCOL_VERSION) {
                     //I don't know this protocol version, ending connection.
@@ -96,11 +101,13 @@ class TCPFluxSrvClient extends Thread {
                 if (code == ProtocolConsts.CODE_END_CONN)
                 {
                     //Respond that you understood, close connection afterwards
+                    System.out.println("Received end code from " + clientIP.getHostAddress());
                     sOut.write(RespondAffirmative.getMessage());
                     break;
                 }
                 else if (code == ProtocolConsts.CODE_TEST)
                 {
+                    System.out.println("Received test code from " + clientIP.getHostAddress());
                     sOut.write(RespondAffirmative.getMessage());
                 }
 
@@ -108,8 +115,10 @@ class TCPFluxSrvClient extends Thread {
             // the client wants to exit
             TcpFluxSrv.remCli(myS);
         } catch (Exception ex) {
+            ex.printStackTrace();
             System.out.println("Error");
         }
+        System.out.println("Ending");
     }
 }
 
